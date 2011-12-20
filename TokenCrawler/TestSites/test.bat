69e0073a855fbde7d5daab99e18abd53c543e63d
Tiago Andrade e Silva <tiagonmas@users.noreply.github.com>
2011-12-20 16:05:16 +0000

    Added sample sites files for testing purposes

---
+++ TokenCrawler/TestSites/test.bat
@@ -0,0 +1,5 @@
+TokenCrawler.exe -token "(\<canvas|createElement\('canvas'\))" -IgnoreCase -File canvas.txt
+pause
+TokenCrawler.exe -token "(msapplication|pinify)" -IgnoreCase -File ie9pin.txt
+pause
+TokenCrawler.exe -token statcounter -IgnoreCase -File statcounter.txt
