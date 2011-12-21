243e45ac988e83fecba2164ecea85a2031a36fd9
tiagonmas_cp <SND\tiagonmas_cp>
2011-12-21 01:03:48 +0000

    added x-ua-compatible sample test

--- TokenCrawler/TestSites/test.bat
+++ TokenCrawler/TestSites/test.bat
@@ -5,3 +5,5 @@ pause
 TokenCrawler.exe /token "(msapplication|pinify)" /File ie9pin.txt
 pause
 TokenCrawler.exe /token statcounter /File statcounter.txt
+pause
+TokenCrawler.exe /token x-ua-compatible /File x-ua-compatible.txt
